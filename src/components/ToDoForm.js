d3420f25e15b94b5c0f321c9078a1407f6169300
Denisse <96150584+Denisse030@users.noreply.github.com>
2023-08-17 01:50:01 -0400

    4th part

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -12,7 +12,7 @@ export const TodoForm = ({addTodo}) => {
       };
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
-    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What is the task today?' />
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What are we doing today?' />
     <button type="submit" className='todo-btn'>Add Task</button>
   </form>
   )
